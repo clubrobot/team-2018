9bd71f5af43bb053389d2fd827d0b88bbae5e6d1
Francois <francois.gauthier-clerc@insa-rennes.fr>
2017-07-19 15:41:27 +0200

    remove temporary files

--- arduino/display/build-nano-atmega328/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-build-nano-atmega328/core/WString.cpp.o: \
- /opt/arduino-1.6.12/hardware/arduino/avr/cores/arduino/WString.cpp \
- /opt/arduino-1.6.12/hardware/arduino/avr/cores/arduino/WString.h
--- arduino/display/build-nano-atmega328/core/new.cpp.d
+++
@@ -1,2 +0,0 @@
-build-nano-atmega328/core/new.cpp.o: \
- /opt/arduino-1.6.12/hardware/arduino/avr/cores/arduino/new.cpp
