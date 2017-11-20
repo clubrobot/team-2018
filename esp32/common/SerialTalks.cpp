f9067fc6c3ee253a437173f6e23792d423e15505
François Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2017-11-20 19:46:56 +0100

    Import from Arduino some libs.

---
+++ esp32/common/SerialTalks.cpp
@@ -0,0 +1,227 @@
+#include "SerialTalks.h"
+#include "Clock.h"
+#include "EEPROM.h"
+
+
+// Global instance
+
+SerialTalks talks;
+
+
+// Built-in instructions
+
+void SerialTalks::PING(SerialTalks& talks, Deserializer& input, Serializer& output)
+{
+	output << true;
+}
+
+void SerialTalks::GETUUID(SerialTalks& inst, Deserializer& input, Serializer& output)
+{
+	char uuid[SERIALTALKS_UUID_LENGTH];
+	talks.getUUID(uuid);
+	output << uuid;
+}
+
+void SerialTalks::SETUUID(SerialTalks& inst, Deserializer& input, Serializer& output)
+{	
+	char uuid[SERIALTALKS_UUID_LENGTH];
+	input >> uuid;
+	talks.setUUID(uuid);
+}
+
+
+// SerialTalks::ostream
+
+void SerialTalks::ostream::begin(SerialTalks& parent, long retcode)
+{
+	m_parent = &parent;
+	m_retcode = retcode;
+}
+
+size_t SerialTalks::ostream::write(uint8_t c)
+{
+	uint8_t buffer[2] = {c, '\0'};
+	return write(buffer, 2);
+}
+
+size_t SerialTalks::ostream::write(const uint8_t *buffer, size_t size)
+{
+	return m_parent->sendback(m_retcode, buffer, size + 1);
+}
+
+
+// SerialTalks
+
+void SerialTalks::begin(Stream& stream)
+{
+
+	//Initialize EEPROM
+	EEPROM.begin(EEPROM_SIZE);
+
+	// Initialize attributes
+	m_stream = &stream;
+	m_connected = false;
+	out.begin(*this, SERIALTALKS_STDOUT_RETCODE);
+	err.begin(*this, SERIALTALKS_STDERR_RETCODE);
+
+	// Initialize UUID stuff
+#ifdef BOARD_UUID
+	setUUID(BOARD_UUID);
+	EEPROM.commit();
+#else
+	char uuid[SERIALTALKS_UUID_LENGTH];
+	if (!getUUID(uuid) || uuid[0] == '\0')
+	{
+		generateRandomUUID(uuid, SERIALTALKS_DEFAULT_UUID_LENGTH);
+		setUUID(uuid);
+	}
+#endif // BOARD_UUID
+
+	// Add UUID accessors
+	bind(SERIALTALKS_PING_OPCODE,    SerialTalks::PING);
+	bind(SERIALTALKS_GETUUID_OPCODE, SerialTalks::GETUUID);
+	bind(SERIALTALKS_SETUUID_OPCODE, SerialTalks::SETUUID);
+
+}
+
+int SerialTalks::sendback(long retcode, const byte* buffer, int size)
+{
+	int count = 0;
+	if (m_stream != 0 && isConnected())
+	{
+		count += m_stream->write(SERIALTALKS_SLAVE_BYTE);
+		count += m_stream->write(byte(sizeof(retcode) + size));
+		count += m_stream->write((byte*)(&retcode), sizeof(retcode));
+		count += m_stream->write(buffer, size);	
+	}
+	return count;
+}
+
+void SerialTalks::bind(byte opcode, Instruction instruction)
+{
+	// Add a command to execute when receiving the specified opcode
+	if (opcode < SERIALTALKS_MAX_OPCODE)
+		m_instructions[opcode] = instruction;
+}
+
+bool SerialTalks::execinstruction(byte* inputBuffer)
+{
+	Deserializer input (inputBuffer);
+	Serializer   output(m_outputBuffer);
+	byte opcode = input.read<byte>();
+	long retcode = input.read<long>();
+	if (m_instructions[opcode] != 0)
+	{
+		m_instructions[opcode](*this, input, output);
+		if (output.buffer > m_outputBuffer)
+			sendback(retcode, m_outputBuffer, output.buffer - m_outputBuffer);
+		return true;
+	}
+	return false;
+}
+
+bool SerialTalks::execute()
+{
+	bool ret = false;
+	int length = m_stream->available();
+
+	long currentTime = millis();
+	if (m_state != SERIALTALKS_WAITING_STATE && currentTime - m_lastTime > 100) // 0.1s timeout
+	{
+		// Abort previous communication
+		m_state = SERIALTALKS_WAITING_STATE;
+	}
+
+	for (int i = 0; i < length; i++)
+	{
+		// Read the incoming byte
+		byte inc = byte(m_stream->read());
+		m_lastTime = currentTime;
+		
+		// Use a state machine to process the above byte
+		switch (m_state)
+		{
+		// An instruction always begin with the Master byte
+		case SERIALTALKS_WAITING_STATE:
+			if (inc == SERIALTALKS_MASTER_BYTE)
+				m_state = SERIALTALKS_INSTRUCTION_STARTING_STATE;
+			continue;
+
+		// The second byte is the instruction size (for example: 'R', '\x02', '\x03', '\x31')
+		case SERIALTALKS_INSTRUCTION_STARTING_STATE:
+			m_bytesNumber  = inc;
+			m_bytesCounter = 0;
+			m_state = (m_bytesNumber <= SERIALTALKS_INPUT_BUFFER_SIZE) ?
+				SERIALTALKS_INSTRUCTION_RECEIVING_STATE :
+				SERIALTALKS_WAITING_STATE;
+			continue;
+
+		// The first instruction byte is the opcode and the others the parameters
+		case SERIALTALKS_INSTRUCTION_RECEIVING_STATE:
+			m_inputBuffer[m_bytesCounter++] = inc;
+			if (m_bytesCounter >= m_bytesNumber)
+			{
+				m_connected = true;
+				ret |= execinstruction(m_inputBuffer);
+				m_state = SERIALTALKS_WAITING_STATE;
+			}
+		}
+	}
+	return ret;
+}
+
+bool SerialTalks::waitUntilConnected(float timeout)
+{
+	Clock clock;
+	clock.restart();
+	while (!isConnected() || (timeout > 0 && clock.getElapsedTime() < timeout))
+		execute();
+	return isConnected();
+}
+
+bool SerialTalks::getUUID(char* uuid)
+{
+
+	for (int i = 0; i < EEPROM_SIZE; i++)
+	{
+		uuid[i] = EEPROM.read(SERIALTALKS_UUID_ADDRESS + i);
+		switch(byte(uuid[i]))
+		{
+		case '\0': return true;
+		case 0xFF: return false;
+		default  : continue;
+		}
+	}
+	return false;
+}
+
+void SerialTalks::setUUID(const char* uuid)
+{
+	int i = 0;
+	do
+		EEPROM.write(SERIALTALKS_UUID_ADDRESS + i, uuid[i]);
+	while(uuid[i++] != '\0');
+	EEPROM.commit();
+}
+
+void SerialTalks::generateRandomUUID(char* uuid, int length)
+{
+	// Initialize the random number generator
+	randomSeed(analogRead(0));
+
+	// Generate the UUID from a list of random hexadecimal numbers
+	for (int i = 0; i < length; i++)
+	{
+		if (i % 5 == 4)
+			uuid[i] = '-';
+		else
+		{
+			long digit = random(16);
+			if (digit < 10)
+				uuid[i] = char('0' + digit);
+			else
+				uuid[i] = char('a' + digit - 10);
+		}
+	}
+	uuid[length] = '\0';
+}
---
+++ esp32/common/SerialTalks.h
@@ -0,0 +1,132 @@
+#ifndef __SERIALTALKS_H__
+#define __SERIALTALKS_H__
+
+#include <Arduino.h>
+#include "serialutils.h"
+
+#ifndef EEPROM_SIZE
+#define EEPROM_SIZE 512
+#endif
+
+
+#ifndef SERIALTALKS_BAUDRATE
+#define SERIALTALKS_BAUDRATE 115200
+#endif
+
+#ifndef SERIALTALKS_INPUT_BUFFER_SIZE
+#define SERIALTALKS_INPUT_BUFFER_SIZE 64
+#endif
+
+#ifndef SERIALTALKS_OUTPUT_BUFFER_SIZE
+#define SERIALTALKS_OUTPUT_BUFFER_SIZE 64
+#endif
+
+#ifndef SERIALTALKS_UUID_ADDRESS
+#define SERIALTALKS_UUID_ADDRESS 0x0000000000
+#endif
+
+#ifndef SERIALTALKS_UUID_LENGTH
+#define SERIALTALKS_UUID_LENGTH	32
+#endif
+
+#ifndef SERIALTALKS_MAX_OPCODE
+#define SERIALTALKS_MAX_OPCODE 0x10
+#endif
+
+#define SERIALTALKS_MASTER_BYTE 'R'
+#define SERIALTALKS_SLAVE_BYTE  'A'
+
+#define SERIALTALKS_DEFAULT_UUID_LENGTH 9
+
+#define SERIALTALKS_PING_OPCODE    0x0
+#define SERIALTALKS_GETUUID_OPCODE 0x1
+#define SERIALTALKS_SETUUID_OPCODE 0x2
+#define SERIALTALKS_STDOUT_RETCODE 0xFFFFFFFF
+#define SERIALTALKS_STDERR_RETCODE 0xFFFFFFFE
+
+
+class SerialTalks
+{
+public: // Public API
+
+	class ostream : public Print
+	{
+	public:
+
+		virtual size_t write(uint8_t);
+		virtual size_t write(const uint8_t *buffer, size_t size);
+
+		template<typename T> ostream& operator<<(const T& object)
+		{
+			print(object);
+			return *this;
+		}
+	
+	protected:
+
+		void begin(SerialTalks& parent, long retcode);
+
+		SerialTalks* m_parent;
+		long         m_retcode;
+
+		friend class SerialTalks;
+	};
+
+	typedef void (*Instruction)(SerialTalks& inst, Deserializer& input, Serializer& output);
+
+	void begin(Stream& stream);
+
+	void bind(byte opcode, Instruction instruction);
+
+	bool execinstruction(byte* inputBuffer);
+	bool execute();
+
+	bool isConnected() const {return m_connected;}
+
+	bool waitUntilConnected(float timeout = -1);
+
+	bool getUUID(char* uuid);
+	void setUUID(const char* uuid);
+
+	static void generateRandomUUID(char* uuid, int length);
+
+	// Public attributes (yes we dare!)
+
+	ostream     out;
+	ostream     err;
+
+protected: // Protected methods
+
+	int sendback(long retcode, const byte* buffer, int size);
+
+	// Attributes
+
+	Stream*     m_stream;
+	bool		m_connected;
+
+	Instruction	m_instructions[SERIALTALKS_MAX_OPCODE];
+
+	byte        m_inputBuffer [SERIALTALKS_INPUT_BUFFER_SIZE];
+	byte        m_outputBuffer[SERIALTALKS_OUTPUT_BUFFER_SIZE];
+
+	enum //     m_state
+	{
+		SERIALTALKS_WAITING_STATE,
+		SERIALTALKS_INSTRUCTION_STARTING_STATE,
+		SERIALTALKS_INSTRUCTION_RECEIVING_STATE,
+	}           m_state;
+	
+	byte        m_bytesNumber;
+	byte        m_bytesCounter;
+	long        m_lastTime;
+
+private:
+
+	static void PING   (SerialTalks& talks, Deserializer& input, Serializer& output);
+	static void GETUUID(SerialTalks& talks, Deserializer& input, Serializer& output);
+	static void SETUUID(SerialTalks& talks, Deserializer& input, Serializer& output);
+};
+
+extern SerialTalks talks;
+
+#endif // __SERIALTALKS_H__
