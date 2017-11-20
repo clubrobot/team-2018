d5a3cf63fdd35c88ed822f541f3d04f0510c3185
François Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2017-11-20 19:47:13 +0100

    Sample project.

---
+++ esp32/test/main.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#include "../common/SerialTalks.h"
+
+
+void setup() {
+    Serial.begin(SERIALTALKS_BAUDRATE);
+    talks.begin(Serial);
+}
+void loop() {
+talks.execute();
+}
+
