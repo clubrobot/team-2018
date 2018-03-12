6e0c19ef9f590df6c256d8d6a250c41b60f165b2
Francois Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-03-12 21:00:01 +0100

    big refactoring ! again.....

--- raspberrypi/robots/colorsensor.py
+++ raspberrypi/robots/colorsensor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+import sys
+sys.path.append("../common/")
 
 import time
 import math
@@ -23,10 +25,10 @@ class ColorSensor(SerialTalksProxy):
 		return r,g,b
 
 	def led_on(self, **kwargs):
-		self.send(LED_ON_OPCODE, BYTE(1))
+		self.send(_LED_ON_OPCODE, BYTE(1))
 	
 	def led_off(self, **kwargs):
-		self.send(LED_OFF_OPCODE, BYTE(1))
+		self.send(_LED_OFF_OPCODE, BYTE(1))
 
 		
 
--- raspberrypi/test.py
+++
@@ -1,9 +0,0 @@
-from components import *
-from watersorter import *
-from waterlauncher import *
-
-t = Manager()
-t.connect()
-w = WaterSorter(t)
-
-l = WaterLauncher(t)
