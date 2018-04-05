e3ef0547607d764e548ca88ceac37c90b7a31328
mlecriva <matlecr35@gmail.com>
2018-04-05 10:55:42 +0200

    add cyclic redundancy check algortihm for checksuming
    Signed-off-by: mlecriva <matlecr35@gmail.com>

---
+++ raspberrypi/common/CRC16.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def CRCprocessBuffer(Buffer):
+   #~~ Generierung der CCITT-CRC16 Checksumme
+   bitrange = range(8) # 8 Bits
+   crcsum   = 0
+   polynom  = 0x1021 #CCITT Polynom
+
+   for byte in Buffer:
+      crcsum ^= byte << 8
+      for bit in bitrange: # Schleife für 8 Bits
+         crcsum <<= 1
+         if crcsum & 0x7FFF0000:
+            #~~ Es gab einen Uebertrag ins Bit-16
+            crcsum = (crcsum & 0x0000FFFF) ^ polynom
+   return crcsum
+
+def CRCcheck(buffer,crc):
+    
+    new_crc = CRCprocessBuffer(buffer)
+
+    #print("new_crc = ")
+    #print(hex(new_crc))
+
+    if crc == new_crc:
+        return True
+    else:
+        return False
