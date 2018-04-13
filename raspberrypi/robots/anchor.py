0fb16777f475d19ed17c5cb1a149330d2be0a573
Timothé <timothe.frignac@yahoo.fr>
2018-04-13 23:33:43 +0200

    Added update_color and get_position functions

--- raspberrypi/robots/anchor.py
+++ raspberrypi/robots/anchor.py
@@ -14,6 +14,8 @@ from components import SerialTalksProxy
 UPDATE_ANCHOR_NUMBER_OPCODE = 0x10
 UPDATE_ANTENNA_DELAY_OPCODE = 0x11
 CALIBRATION_ROUTINE_OPCODE = 0x12
+UPDATE_COLOR_OPCODE = 0x13
+GET_POSITION_OPCODE = 0x14
 
 
 class Anchor(SerialTalksProxy):
@@ -33,5 +35,14 @@ class Anchor(SerialTalksProxy):
 	def calibrate(self,real_distance, timeout):
 		self.send(CALIBRATION_ROUTINE_OPCODE,INT(real_distance),ULONG(timeout))
 		print(self.getout(timeout=1))
+
+#green : 0, orange : 1
+	def update_color(self,real_distance, color):
+		self.send(UPDATE_COLOR_OPCODE,INT(color))
+
+	def get_potision(self,real_distance):
+		output = self.execute(GET_POSITION_OPCODE)
+		x, y = output.read(INT, INT)
+		return x, y		
 	
 
