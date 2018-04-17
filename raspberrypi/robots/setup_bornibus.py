224406b708b14d9c68f03bef017db297ca9b06d5
François Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-04-17 14:37:25 +0200

    fix Geogebra import

--- raspberrypi/robots/setup_bornibus.py
+++ raspberrypi/robots/setup_bornibus.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from common.roadmap import RoadMap
-from common.geogebra import GeoGebra
+from common.geogebra import Geogebra
 # Check for the Rapsberry Pi address
 # It looks for a file in the current directory, whose name is
 # a valid IP address
@@ -10,13 +10,13 @@ from common.geogebra import GeoGebra
 
 # Import robot stuff
 
-from arduino.components         import *
-from arduino.wheeledbase        import *
-from arduino.waterlauncher      import *
-from arduino.watersorter        import *
-from arduino.display            import *
-from arduino.bee                import *
-from arduino.sensors            import *
+from arduino.components import *
+from arduino.wheeledbase import *
+from arduino.waterlauncher import *
+from arduino.watersorter import *
+from arduino.display import *
+from arduino.bee import *
+from arduino.sensors import *
 
 # Define temporary modules
 
@@ -24,53 +24,47 @@ from arduino.sensors            import *
 MAX_VEL = 500
 MAX_ROT = 6
 
-
 try:
-	m = Manager(hostname)
-	m.connect(10)
+    m = Manager(hostname)
+    m.connect(10)
 except:
-	raise
-	m = Manager()
-	m.connect(10)
+    raise
+    m = Manager()
+    m.connect(10)
 
 try:
-	b = WheeledBase(m)
-except :
-	print('\'wheeledbase\' not connected')
-
-try:
-	d = WaterSorter(m)
-	l = WaterLauncher(m)
-	a = BeeActuator(m)
-except :
-	print('\'wattershooter\' not connected')
-
-try:
-#	l = Module(m, 'display')
-	ssd = SevenSegments(m)
-	led1 = LEDMatrix(m, 1)
-	led2 = LEDMatrix(m, 2)
+    b = WheeledBase(m)
 except:
-	print('\'display\' not connected')
-try:
-	s_lat = Sensors(m,"sensorsLat")
-except :
-	print('\'sensors lat\' not connected')
+    print('\'wheeledbase\' not connected')
 
 try:
-	s_front = Sensors(m,"sensorsAv")
+    d = WaterSorter(m)
+    l = WaterLauncher(m)
+    a = BeeActuator(m)
 except:
-	print('\'sensors front\' not connected')
+    print('\'wattershooter\' not connected')
 
 try:
-	s_back = Sensors(m,"sensorsAr")
+    #	l = Module(m, 'display')
+    ssd = SevenSegments(m)
+    led1 = LEDMatrix(m, 1)
+    led2 = LEDMatrix(m, 2)
 except:
-	print('\'sensors back\' not connected')
+    print('\'display\' not connected')
+try:
+    s_lat = Sensors(m, "sensorsLat")
+except:
+    print('\'sensors lat\' not connected')
 
-	
+try:
+    s_front = Sensors(m, "sensorsAv")
+except:
+    print('\'sensors front\' not connected')
 
+try:
+    s_back = Sensors(m, "sensorsAr")
+except:
+    print('\'sensors back\' not connected')
 
-geo = GeoGebra('bornibus.ggb')
+geo = Geogebra('bornibus.ggb')
 rm = RoadMap.load(geo)
-
-
