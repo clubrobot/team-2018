59e7ee678e2a3fcc58547d92cb1d977bf13935cd
Francois Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-04-21 21:16:37 +0200

    Add lock's on getter and setter.

--- raspberrypi/robots/beacons_manager.py
+++ raspberrypi/robots/beacons_manager.py
@@ -2,29 +2,39 @@
 # coding: utf-8
 
 from time import sleep
-from threading import Thread, Event
+from threading import Thread, Event, RLock
 from beacons.anchor import Anchor
 from common.geogebra import Geogebra
 
 REDUCE_CONSTANT = 0.05
 INCREASE_CONSTANT = 0.10
+
 class Area:
     def __init__(self, points_list):
         self.points = points_list
         self.value = 0
+        self.lock = RLock()
     def reduce(self):
+        self.lock.acquire()
         self.value = max(self.value-REDUCE_CONSTANT,0)
+        self.lock.release()
 
     def increase(self, x, y):
+        self.lock.acquire()
         for i in range(len(self.points)):
-            ((x_1, y_1), (x_2, y_2)) = (self.points[i], self.points[(i+1)%(len(self.points)+1)])
+            ((x_1, y_1), (x_2, y_2)) = (self.points[i], self.points[(i+1)%(len(self.points))])
             vect = (x_2-x_1)*(y-y_1) - (y_2-y_1)*(x-x_1)
             if vect<0:
-
-                print("aie", i, (i+1)%(len(self.points)+1))
-                print(((x_1, y_1), (x_2, y_2)) )
+                self.lock.release()
                 return
+        self.lock.release()
         self.value = min(self.value+INCREASE_CONSTANT,1)
+
+    def get_value(self):
+        self.lock.acquire()
+        result = self.value
+        self.lock.release()
+        return  result
 
 
 class BeaconsManagement(Thread):
@@ -47,7 +57,7 @@ class BeaconsManagement(Thread):
 
     def get_area_value(self, name):
         try:
-            return  self.areas[name].value
+            return  self.areas[name].get_value()
         except KeyError:
             raise RuntimeError("No area called {}.".format(name))
 
