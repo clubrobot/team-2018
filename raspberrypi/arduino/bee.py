936f4acc26b80ef618faedc1c6c08c68b49403db
François Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-04-23 22:07:13 +0200

    update constant.

--- raspberrypi/arduino/bee.py
+++ raspberrypi/arduino/bee.py
@@ -12,8 +12,8 @@ _WRITE_BEEACTIVATOR_OPCODE		=   0x27
 class BeeActuator(SecureSerialTalksProxy):
 	def __init__(self,parent, uuid='watershooter'):
 		SecureSerialTalksProxy.__init__(self,parent, uuid, dict())
-		self.closed_position = 165
-		self.open_position = 75
+		self.closed_position = 0
+		self.open_position = 90
 	
 	def write(self, value):
 		self.send(_WRITE_BEEACTIVATOR_OPCODE,INT(value))
